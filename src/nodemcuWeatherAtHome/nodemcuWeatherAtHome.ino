3044bf872937ad6cb0f17429472a8807832dea53
Christoph Jost <c.jost@micromata.de>
2022-02-17 17:51:19 +0100

    Font placement

--- src/nodemcuWeatherAtHome/nodemcuWeatherAtHome.ino
+++ src/nodemcuWeatherAtHome/nodemcuWeatherAtHome.ino
@@ -9,10 +9,26 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 HTU21D sensor;
 
+float temperature = 100;
+float humidity = -100;
+
 void setup() {
  Serial.begin(9600); /* begin serial for debug */
  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
 
+ initDisplay();
+
+  sensor.begin();
+}
+
+void loop() {
+  readSensorData();
+  renderDisplay();
+  
+  delay(5000);
+}
+
+void initDisplay() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
@@ -24,28 +40,38 @@ void setup() {
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // Display static text
+  display.setTextSize(3);
+  display.setCursor(0, (SCREEN_HEIGHT/2) - 10);
+  display.println("Loading...");
   display.display();
-
-  sensor.begin();
 }
 
-void loop() {
-  if(sensor.measure()) {
-    float temperature = sensor.getTemperature();
-    float humidity = sensor.getHumidity();
-
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("Temperature (C): ");
-    display.println(temperature);
-
-    display.print("Humidity (%RH): ");
-    display.println(humidity);
-    display.display();
-
+void readSensorData() {
+  if(sensor.measure()){
+    temperature = sensor.getTemperature();
+    humidity = sensor.getHumidity();
   }
+}
 
-  delay(5000);
+void renderDisplay() {
+
+  display.clearDisplay();
+  display.dim(true);
+
+  display.drawLine(0, 14, SCREEN_WIDTH, 14, WHITE);
+     
+  display.setTextSize(3);
+  display.setCursor(0, (SCREEN_HEIGHT/2) - 10);
+    
+  display.print(temperature);
+  display.println(" C");
+
+  display.drawLine(0, 50, SCREEN_WIDTH, 50, WHITE);
+    
+  display.setTextSize(1);
+  display.setCursor(0, 55);
+  display.print("(%RH): ");
+  display.println(humidity);
+
+  display.display();
 }
